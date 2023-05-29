d18ffb5ea72270546a560de49407b5318f5aefa6
Remi Cresson <remi.cresson@inrae.fr>
2023-05-29 22:53:08 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = ["requests", "pydantic", "urllib3", "tqdm"]
 
 setup(
     name="theia-picker",
-    version="1.0.3",
+    version="1.1.0",
     description="Theia picker",
     python_requires=">=3.8",
     author="Remi Cresson",
