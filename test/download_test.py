513c2af71781b55df21b04977fd79567c44484a0
Cresson Remi <remi.cresson@irstea.fr>
2024-02-02 16:07:05 +0100

    Update file download_test.py

--- test/download_test.py
+++ test/download_test.py
@@ -7,6 +7,8 @@ import theia_picker
 username = os.environ["THEIA_IDENT"]
 password = os.environ["THEIA_PASS"]
 
+test_zip = False
+
 with tempfile.TemporaryDirectory() as output_dir:
     # Login
     cat = theia_picker.TheiaCatalog(
@@ -28,24 +30,30 @@ with tempfile.TemporaryDirectory() as output_dir:
     for feat in features:
         assert feat.properties.product_identifier == \
                "SENTINEL2A_20220114-103855-001_L2A_T31TFJ_D"
-        files = feat.list_files_in_archive()
-        assert len(files) == 527
-        for file in files:
-            if any(pattern in file for pattern in patterns):
-                feat.download_single_file(
-                    filename=file,
-                    download_dir=output_dir
-                )
-                out_file = os.path.join(output_dir, file)
-                assert os.path.isfile(out_file)
-    print("Download single file OK")
+        if test_zip:
+            files = feat.list_files_in_archive()
+            assert len(files) == 527
+            for file in files:
+                if any(pattern in file for pattern in patterns):
+                    feat.download_single_file(
+                        filename=file,
+                        download_dir=output_dir
+                    )
+                    out_file = os.path.join(output_dir, file)
+                    assert os.path.isfile(out_file)
+            print("Download single file OK")
+
+        # Download archive
+        feat.download_archive("/tmp/")
+        print("Download archive OK")
 
     # Download files batch
     feats = cat.search(
         tile_name="T31TEJ", start_date="14/01/2021", level="LEVEL2A"
     )
-    for f in feats:
-        f.download_files(
-            matching=["FRE_B4.tif", "FRE_B8.tif"], download_dir="/tmp"
-        )
-    print("Download multiple files OK")
+    if test_zip:
+        for f in feats:
+            f.download_files(
+                matching=["FRE_B4.tif", "FRE_B8.tif"], download_dir="/tmp"
+            )
+        print("Download multiple files OK")
